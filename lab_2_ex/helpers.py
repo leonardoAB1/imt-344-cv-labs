fad0dbb09ac85ba796b197067f891476943136b2
Leonardo Achá <leonardoacha@hotmail.com>
2023-04-05 08:09:55 -0400

    region growing implemented on helpers file

--- lab_2_ex/helpers.py
+++ lab_2_ex/helpers.py
@@ -32,4 +32,44 @@ def load_dataset(image_dir):
 
     return im_list
 
-
+def region_growing(img, seed, tolerance=0.1, color=255):
+    """
+    Segment an image using region growing.
+    
+    Args:
+        img (np.ndarray): The input image.
+        seed (tuple): The pixel coordinates (x, y) of the seed point.
+        tolerance (float): Allowed difference between seed value and neighborhood
+        color (int) = mask color
+    
+    Returns:
+        np.ndarray: The segmented image mask.
+    """
+    # Initialize segmented image with zeros
+    segmented = np.zeros_like(img)
+    
+    # Initialize queue with seed point
+    queue = [seed]
+    
+    # Get seed point color
+    seed_color = img[seed]
+    
+    # Loop until the queue is empty
+    while queue:
+        # Get next pixel from queue
+        pixel = queue.pop(0)
+        
+        # Check if pixel is within image bounds
+        if (0 <= pixel[0] < img.shape[0]) and (0 <= pixel[1] < img.shape[1]):
+            # Check if pixel is unsegmented and similar to seed color
+            if segmented[pixel[0], pixel[1]] == 0 and np.allclose(img[pixel], seed_color, rtol=tolerance, atol=tolerance):
+                # Segment pixel
+                segmented[pixel[0], pixel[1]] = color
+                
+                # Add neighboring pixels to queue
+                queue.append((pixel[0] - 1, pixel[1]))
+                queue.append((pixel[0] + 1, pixel[1]))
+                queue.append((pixel[0], pixel[1] - 1))
+                queue.append((pixel[0], pixel[1] + 1))
+    
+    return segmented
